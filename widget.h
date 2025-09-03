7c6be2eee95aa832b596ae4db77d4f7ca8c6f761
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2025-09-03 09:46:00 +0530

    Added a basic simulator

--- widget.h
+++ widget.h
@@ -2,6 +2,10 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QTimer>
+#include <cstdlib>
+#include <ctime>
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Widget; }
@@ -17,5 +21,49 @@ public:
 
 private:
     Ui::Widget *ui;
+
+    QTimer *timer;                    ///< Timer for simulation updates
+
+    long simTick;
+
+    // Sensor Bearing
+    qreal currentSensorBearing;
+    qreal prevSensorBearing;
+
+    // Own Ship Info
+    qreal currentOwnShipBearing;
+    qreal currentShipSpeed;
+    qreal prevOwnShipBearing;
+    qreal prevShipSpeed;
+    
+    // Selected Track Info
+    qreal currentSelectedTrackRange;
+    qreal currentSelectedTrackBearing;
+    qreal currentSelectedTrackSpeed;
+
+    qreal prevSelectedTrackRange;
+    qreal prevSelectedTrackBearing;
+    qreal prevSelectedTrackSpeed;
+
+
+    // Adopted Track Info
+    qreal currentAdoptedTrackRange;
+    qreal currentAdoptedTrackBearing;
+    qreal currentAdoptedTrackSpeed;
+
+    qreal prevAdoptedTrackRange;
+    qreal prevAdoptedTrackBearing;
+    qreal prevAdoptedTrackSpeed;
+
+
+private slots:
+    /**
+     * @brief Updates simulation state every timer interval
+     * 
+     * Called every 2 seconds to update target position, bearing, range,
+     * and bearing rate calculations. Triggers widget repaint.
+     */
+    void updateSimulation();
+
 };
 #endif // WIDGET_H
