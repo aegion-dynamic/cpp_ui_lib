3736120d3e1bed08504e5f84193d2f1f96a7fd55
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2025-10-01 16:02:31 +0530

    Fixed the bug that is causing the crash on linux

--- graphcontainer.cpp
+++ graphcontainer.cpp
@@ -1,8 +1,8 @@
 #include "graphcontainer.h"
 #include <QDebug>
 
-GraphContainer::GraphContainer(QWidget *parent, bool showTimelineView, QTimer *timer)
-    : QWidget{parent}, m_showTimelineView(showTimelineView), m_timer(timer), m_ownsTimer(false), m_timelineWidth(150), m_graphViewSize(80, 300), currentDataOption(GraphType::BDW)
+GraphContainer::GraphContainer(QWidget* parent, bool showTimelineView, QTimer* timer)
+    : QWidget{ parent }, m_showTimelineView(showTimelineView), m_timer(timer), m_ownsTimer(false), m_timelineWidth(150), m_graphViewSize(80, 300), currentDataOption(GraphType::BDW)
 {
 
     // If the timer is not provided, create a default 1-second timer
@@ -44,7 +44,6 @@ GraphContainer::GraphContainer(QWidget *parent, bool showTimelineView, QTimer *t
     // Add ComboBox, ZoomPanel, and WaterfallGraph to left layout
     m_leftLayout->addWidget(m_comboBox);
     m_leftLayout->addWidget(m_zoomPanel);
-    m_leftLayout->addWidget(m_waterfallGraph, 1); // Add stretch factor of 1 to make it expand
 
     // Add left layout to main layout with stretch factor
     m_mainLayout->addLayout(m_leftLayout, 1); // Give stretch factor of 1 to left layout
@@ -205,13 +204,13 @@ void GraphContainer::updateTotalContainerSize()
 
 // Data point methods implementation
 
-void GraphContainer::setData(const std::vector<qreal> &yData, const std::vector<QDateTime> &timestamps)
+void GraphContainer::setData(const std::vector<qreal>& yData, const std::vector<QDateTime>& timestamps)
 {
     waterfallData.setData(yData, timestamps);
     initializeZoomPanelLimits();
 }
 
-void GraphContainer::setData(const WaterfallData &data)
+void GraphContainer::setData(const WaterfallData& data)
 {
     waterfallData = data;
     initializeZoomPanelLimits();
@@ -224,13 +223,13 @@ void GraphContainer::clearData()
     initializeZoomPanelLimits();
 }
 
-void GraphContainer::addDataPoint(qreal yValue, const QDateTime &timestamp)
+void GraphContainer::addDataPoint(qreal yValue, const QDateTime& timestamp)
 {
     waterfallData.addDataPoint(yValue, timestamp);
     initializeZoomPanelLimits();
 }
 
-void GraphContainer::addDataPoints(const std::vector<qreal> &yValues, const std::vector<QDateTime> &timestamps)
+void GraphContainer::addDataPoints(const std::vector<qreal>& yValues, const std::vector<QDateTime>& timestamps)
 {
     waterfallData.addDataPoints(yValues, timestamps);
     initializeZoomPanelLimits();
@@ -246,7 +245,7 @@ std::vector<std::pair<qreal, QDateTime>> GraphContainer::getDataWithinYExtents(q
     return waterfallData.getDataWithinYExtents(yMin, yMax);
 }
 
-std::vector<std::pair<qreal, QDateTime>> GraphContainer::getDataWithinTimeRange(const QDateTime &startTime, const QDateTime &endTime) const
+std::vector<std::pair<qreal, QDateTime>> GraphContainer::getDataWithinTimeRange(const QDateTime& startTime, const QDateTime& endTime) const
 {
     return waterfallData.getDataWithinTimeRange(startTime, endTime);
 }
@@ -277,7 +276,7 @@ void GraphContainer::redrawWaterfallGraph()
 
 // Data options management implementation
 
-void GraphContainer::addDataOption(const GraphType graphType, WaterfallData &dataSource)
+void GraphContainer::addDataOption(const GraphType graphType, WaterfallData& dataSource)
 {
     QString title = graphTypeToString(graphType);
     dataOptions[graphType] = &dataSource;
@@ -367,14 +366,14 @@ GraphType GraphContainer::getCurrentDataOption() const
 std::vector<GraphType> GraphContainer::getAvailableDataOptions() const
 {
     std::vector<GraphType> options;
-    for (const auto &pair : dataOptions)
+    for (const auto& pair : dataOptions)
     {
         options.push_back(pair.first);
     }
     return options;
 }
 
-WaterfallData *GraphContainer::getDataOption(const GraphType graphType)
+WaterfallData* GraphContainer::getDataOption(const GraphType graphType)
 {
     auto it = dataOptions.find(graphType);
     return (it != dataOptions.end()) ? it->second : nullptr;
@@ -388,7 +387,7 @@ bool GraphContainer::hasDataOption(const GraphType graphType) const
 void GraphContainer::updateComboBoxOptions()
 {
     m_comboBox->clear();
-    for (const auto &pair : dataOptions)
+    for (const auto& pair : dataOptions)
     {
         m_comboBox->addItem(graphTypeToString(pair.first));
     }
@@ -407,28 +406,28 @@ void GraphContainer::setupEventConnections()
 {
     // Connect ComboBox data source selection
     connect(m_comboBox, &QComboBox::currentTextChanged,
-            this, &GraphContainer::onDataOptionChanged);
+        this, &GraphContainer::onDataOptionChanged);
 
     // Connect WaterfallGraph selection events
     connect(m_waterfallGraph, &WaterfallGraph::SelectionCreated,
-            this, &GraphContainer::onSelectionCreated);
+        this, &GraphContainer::onSelectionCreated);
 
     // Connect ZoomPanel value changes
     connect(m_zoomPanel, &ZoomPanel::valueChanged,
-            this, &GraphContainer::onZoomValueChanged);
+        this, &GraphContainer::onZoomValueChanged);
 
     // Connect TimelineView interval changes (if timeline view exists)
     if (m_timelineView)
     {
         connect(m_timelineView, &TimelineView::TimeIntervalChanged,
-                this, &GraphContainer::onTimeIntervalChanged);
+            this, &GraphContainer::onTimeIntervalChanged);
     }
 
     // Connect TimeSelectionVisualizer clear button events
     if (m_timelineSelectionView)
     {
         connect(m_timelineSelectionView, &TimeSelectionVisualizer::timeSelectionsCleared,
-                this, &GraphContainer::onClearTimeSelectionsButtonClicked);
+            this, &GraphContainer::onClearTimeSelectionsButtonClicked);
     }
 
     qDebug() << "GraphContainer: All event connections established";
@@ -444,12 +443,12 @@ void GraphContainer::setupEventConnectionsForWaterfallGraph()
 
     // Connect WaterfallGraph selection events
     connect(m_waterfallGraph, &WaterfallGraph::SelectionCreated,
-            this, &GraphContainer::onSelectionCreated);
+        this, &GraphContainer::onSelectionCreated);
 
     qDebug() << "GraphContainer: Event connections established for waterfall graph";
 }
 
-WaterfallGraph *GraphContainer::createWaterfallGraph(GraphType graphType)
+WaterfallGraph* GraphContainer::createWaterfallGraph(GraphType graphType)
 {
     switch (graphType)
     {
@@ -488,7 +487,7 @@ void GraphContainer::initializeWaterfallGraph(GraphType graphType)
     m_waterfallGraph = createWaterfallGraph(graphType);
 
     // Set up the data source
-    WaterfallData *dataSource = nullptr;
+    WaterfallData* dataSource = nullptr;
     auto it = dataOptions.find(graphType);
     if (it != dataOptions.end())
     {
@@ -524,7 +523,7 @@ void GraphContainer::initializeWaterfallGraph(GraphType graphType)
     setupEventConnectionsForWaterfallGraph();
 }
 
-void GraphContainer::subscribeToIntervalChange(QObject *subscriber, const char *slot)
+void GraphContainer::subscribeToIntervalChange(QObject* subscriber, const char* slot)
 {
     if (subscriber && slot)
     {
@@ -574,7 +573,7 @@ void GraphContainer::updateTimeInterval(TimeInterval interval)
     }
 }
 
-void GraphContainer::onSelectionCreated(const TimeSelectionSpan &selection)
+void GraphContainer::onSelectionCreated(const TimeSelectionSpan& selection)
 {
     qDebug() << "GraphContainer: Selection created from" << selection.startTime.toString() << "to" << selection.endTime.toString();
 
@@ -619,7 +618,7 @@ void GraphContainer::testSelectionRectangle()
     }
 }
 
-void GraphContainer::setCurrentTime(const QTime &time)
+void GraphContainer::setCurrentTime(const QTime& time)
 {
     qDebug() << "GraphContainer: Setting current time to" << time.toString();
     if (m_timelineSelectionView)
@@ -633,7 +632,7 @@ void GraphContainer::setCurrentTime(const QTime &time)
     }
 }
 
-void GraphContainer::addTimeSelection(const TimeSelectionSpan &selection)
+void GraphContainer::addTimeSelection(const TimeSelectionSpan& selection)
 {
     qDebug() << "GraphContainer: Adding time selection from" << selection.startTime.toString() << "to" << selection.endTime.toString();
 
@@ -692,7 +691,7 @@ void GraphContainer::initializeZoomPanelLimits()
     }
 
     // Get the current data source (either selected option or default waterfallData)
-    WaterfallData *currentDataSource = nullptr;
+    WaterfallData* currentDataSource = nullptr;
     auto it = dataOptions.find(currentDataOption);
     if (it != dataOptions.end())
     {
@@ -719,7 +718,7 @@ void GraphContainer::initializeZoomPanelLimits()
     m_zoomPanel->setRightLabelValue(dataMax);
 
     qDebug() << "GraphContainer: Zoom panel limits initialized - Min:" << dataMin
-             << "Center:" << centerValue << "Max:" << dataMax << "- Zoom reset to 50%";
+        << "Center:" << centerValue << "Max:" << dataMax << "- Zoom reset to 50%";
 }
 
 void GraphContainer::onZoomValueChanged(ZoomBounds bounds)
@@ -731,7 +730,7 @@ void GraphContainer::onZoomValueChanged(ZoomBounds bounds)
     }
 
     qDebug() << "GraphContainer: Received zoom value changed signal - Lower:" << bounds.lowerbound
-             << "Upper:" << bounds.upperbound;
+        << "Upper:" << bounds.upperbound;
 
     // Convert normalized bounds to actual data values using zoom panel label values
     qreal leftLabel = m_zoomPanel->getLeftLabelValue();
@@ -742,7 +741,7 @@ void GraphContainer::onZoomValueChanged(ZoomBounds bounds)
     qreal actualUpperBound = leftLabel + bounds.upperbound * (rightLabel - leftLabel);
 
     qDebug() << "GraphContainer: Converted to actual data values - Lower:" << actualLowerBound
-             << "Upper:" << actualUpperBound;
+        << "Upper:" << actualUpperBound;
 
     // Set the custom Y range based on actual data bounds
     m_waterfallGraph->setCustomYRange(actualLowerBound, actualUpperBound);
@@ -760,7 +759,7 @@ void GraphContainer::onClearTimeSelectionsButtonClicked()
 }
 
 // Chevron label control methods implementation
-void GraphContainer::setChevronLabel1(const QString &label)
+void GraphContainer::setChevronLabel1(const QString& label)
 {
     if (m_timelineView)
     {
@@ -773,7 +772,7 @@ void GraphContainer::setChevronLabel1(const QString &label)
     }
 }
 
-void GraphContainer::setChevronLabel2(const QString &label)
+void GraphContainer::setChevronLabel2(const QString& label)
 {
     if (m_timelineView)
     {
@@ -786,7 +785,7 @@ void GraphContainer::setChevronLabel2(const QString &label)
     }
 }
 
-void GraphContainer::setChevronLabel3(const QString &label)
+void GraphContainer::setChevronLabel3(const QString& label)
 {
     if (m_timelineView)
     {
