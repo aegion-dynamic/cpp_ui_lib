22550a48340228de65cd1ff47a68ac7ebb7ac66e
Muzm1l <syedmuzammil16334@gmail.com>
2025-11-18 00:44:57 +0530

    qt5 compatable

--- waterfallgraph.h
+++ waterfallgraph.h
@@ -19,7 +19,7 @@
 #include <QPainterPath>
 #include <QPalette>
 #include <QPolygonF>
-#include <QEnterEvent>
+#include <QEvent>
 #include <QResizeEvent>
 #include <QShowEvent>
 #include <QTime>
@@ -84,7 +84,7 @@ protected:
     void mousePressEvent(QMouseEvent *event) override;
     void mouseReleaseEvent(QMouseEvent *event) override;
     // Override mouse move to track cursor for crosshair
-    void enterEvent(QEnterEvent *event) override;
+    void enterEvent(QEvent *event) override;
     void leaveEvent(QEvent *event) override;
     void mouseMoveEvent(QMouseEvent *event) override;
 
